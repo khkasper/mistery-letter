6e5d1d9ef3a78f7106972835d11eebb191b912a9
Kristiano <kristianokasper@gmail.com>
2021-07-11 17:01:23 -0300

    Confereindo randomizaçao classes e condicionais de teste

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const letterText = document.querySelector('#carta-texto');
 const letterBttn = document.querySelector('#criar-carta');
 const misteryLetter = document.querySelector('#carta-gerada');
-const style = ['newspaper', 'magazine1', 'magazine2'];
-const size = ['medium', 'big', 'reallybig'];
-const rotate = ['rotateleft', 'rotateright'];
-const skew = ['skewleft', 'skewright'];
+const letterCounter = document.querySelector('carta-contador');
+const styleLetter = ['newspaper', 'magazine1', 'magazine2'];
+const sizeLetter = ['medium', 'big', 'reallybig'];
+const rotateLetter = ['rotateleft', 'rotateright'];
+const skewLetter = ['skewleft', 'skewright'];
 
 function misteryLetterMkr() {
   letterBttn.addEventListener('click', () => {
-    if (letterText.value === '' || letterText.value === ' ') {
+    if (letterText.value === '') {
       misteryLetter.innerText = 'Por favor, digite o conteúdo da carta.';
     }
-    const letterSize = letterText.value.split(' ');
-    for (let index = 0; index < letterSize.length; index++) {
+    if (misteryLetter.innerHTML !== '') {
+      misteryLetter.innerHTML = '';
+    }
+    const letterContent = letterText.value.split(' ');
+    for (let index = 0; index < letterContent.length; index++) {
       const spanMkr = document.createElement('span');
-      spanMkr.innerText = letterSize[index];
+      spanMkr.innerText = letterContent[index];
+      spanMkr.classList.add(styleLetter[Math.ceil(Math.random() * styleLetter.length - 1)]);
+      spanMkr.classList.add(sizeLetter[Math.ceil(Math.random() * styleLetter.length - 1)]);
+      spanMkr.classList.add(rotateLetter[Math.ceil(Math.random() * styleLetter.length - 1)]);
+      spanMkr.classList.add(skewLetter[Math.ceil(Math.random() * styleLetter.length - 1)]);
       misteryLetter.appendChild(spanMkr);
     }
   });
