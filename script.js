da7e215e996499b0236f6854723131f7678df764
Kristiano <kristianokasper@gmail.com>
2021-07-11 16:03:48 -0300

    Condicional carta vazia

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const size = ['medium', 'big', 'reallybig'];
 const rotate = ['rotateleft', 'rotateright'];
 const skew = ['skewleft', 'skewright'];
 const spanMkr = document.createElement('span');
-const span = document.querySelectorAll('span');
 
 function misteryLetterMkr() {
   letterBttn.addEventListener('click', () => {
+    if (letterText.value === '') {
+      misteryLetter.innerText = 'Por favor, digite o conteúdo da carta.';
+    }
     const letterSize = letterText.value.split(' ');
     for (let index = 0; index < letterSize.length; index++) {
       spanMkr.innerText = letterSize[index];
